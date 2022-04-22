3f2079848bd8c7c5048b7058c1346d8d3f0b5408
John Connolly <jcconnol4@gmail.com>
2022-04-22 18:47:53 -0500

    clean up

--- popup.js
+++ popup.js
@@ -5,8 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get(["pageObject"], async function(result) {
         var getImageInfoButton = document.getElementsByClassName('get-image-info-button')[0];
 
-        //var numericRegExp = new RegExp('^((?:NaN|-?(?:(?:\\d+|\\d*\\.\\d+)(?:[E|e][+|-]?\\d+)?|Infinity)))$')
-
         var currentURL = null;
 
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
@@ -23,145 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
         
-        //Helper functions for sorting
-        function isNumeric (value) {
-            return numericRegExp.test(String(value))
-        }
-
-        function toArray (value) {
-            if (!value) {
-                return []
-            }
-            
-            if (Array.isArray(value)) {
-                return value
-            }
-            
-            if (value instanceof window.NodeList || value instanceof window.HTMLCollection) {
-                return Array.prototype.slice.call(value)
-            }
-            
-            return [ value ]
-        }
-
-        function sortTable (table, ordering) {
-            console.log("first table")
-            var thead = table.querySelector('thead')
-            var tbody = table.querySelector('tbody')
-            var rows = toArray(tbody.rows)
-            var headers = toArray(thead.rows[0].cells)
-
-            var current = toArray(thead.querySelectorAll('.sorting_desc, .sorting_asc'))
-            
-            current.filter(function (item) { return !!item }).forEach(function (item) {
-                item.classList.remove('sorting_desc')
-                item.classList.remove('sorting_asc')
-            })
-            
-            headers.filter(function (item) { return !!item }).forEach(function (header) {
-                header.classList.remove('sorting_desc')
-                header.classList.remove('sorting_asc')
-            })
-            
-            ordering.forEach(function (order) {
-                var index = order.idx
-                var direction = order.dir || 'asc'
-                
-                var header = headers[index]
-                header.classList.add('sorting_' + direction)
-            })
-            
-            rows.sort(function sorter (a, b) {
-                var i = 0
-                var order = ordering[i]
-                var length = ordering.length
-                var aText
-                var bText
-                var result = 0
-                var dir
-                
-                while (order && result === 0) {
-                    dir = order.dir === 'desc' ? -1 : 1
-
-
-                    if(!a.cells[order.idx]){
-                        break;
-                    }
-
-                    if(!b.cells[order.idx]){
-                        break;
-                    }
-
-                    // if(!('textContent' in a.cells[order.idx])){
-                    //     break;
-                    // }
-
-                    // if(!('textContent' in b.cells[order.idx])){
-                    //     break;
-                    // }
-                    
-                    aText = a.cells[order.idx].textContent
-                    bText = b.cells[order.idx].textContent
-
-                    if (isNumeric(aText) && isNumeric(bText)) {
-                        result = dir * (parseFloat(aText) - parseFloat(bText))
-                    } else {
-                        result = dir * aText.localeCompare(bText)
-                    }
-                    
-                    i += 1
-                    order = ordering[i]
-                }
-                
-                return result
-            }).forEach(function each (row) {
-                tbody.appendChild(row)
-            })
-        }
-
-        function find (array, predicate) {
-            return toArray(array).filter(predicate)[0]
-        }
-
-        function initSortTable (table) {
-            var thead = table.querySelector('thead')
-            var ordering = [{idx:2,dir:'asc'},{idx:1,dir:'asc'}]
-            
-            table.__ordering = ordering
-            
-            thead.addEventListener('click', function onClick (event) {
-                var src = event.target || event.srcElement
-                var tagName = src.tagName.toLowerCase()
-                
-                if (tagName !== 'th') {
-                return
-                }
-                
-                if (!event.shiftKey) {
-                table.__ordering = [
-                    {
-                    idx: src.cellIndex,
-                    dir: src.classList.contains('sorting_asc') ? 'desc' : 'asc'
-                    }
-                ]
-                } else {
-                var order = find(table.__ordering, function (item) {
-                    return item.idx === src.cellIndex
-                })
-                
-                if (order) {
-                    order.dir = order.dir === 'asc' ? 'desc' : 'asc'
-                } else {
-                    table.__ordering.push({
-                    idx: src.cellIndex,
-                    dir: 'asc'
-                    })
-                }
-                }
-                
-                sortTable(table, table.__ordering)
-            }, false)
-        }
+        
     });
 });
 
@@ -172,30 +32,6 @@ window.addEventListener('load', function(event){
         sendMessage
     )
 });
-
-function sendMessage(){
-    var getImageInfoButton = document.getElementsByClassName('get-image-info-button')[0];
-
-    getImageInfoButton.addEventListener('click', function() {
-
-        getImageInfoButton.disabled = true;
-
-        var params = {
-            active: true,
-            currentWindow: true
-        }
-
-        var messageTo = {
-            subject: "DOMInfo",
-            from: "imgs"
-        }
-
-        chrome.tabs.query(params, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, messageTo);
-        });
-
-    }, false);
-}
 
 chrome.runtime.onMessage.addListener(
     async function(request, sender) {
@@ -283,154 +119,32 @@ chrome.runtime.onMessage.addListener(
                 });
             });
         }
-
-        var numericRegExp = new RegExp('^((?:NaN|-?(?:(?:\\d+|\\d*\\.\\d+)(?:[E|e][+|-]?\\d+)?|Infinity)))$')
-
-        // initSortTable(document.querySelector('table'))
-
-        // getImageInfoButton.disabled = false;
-        
-        //Helper functions for sorting
-        function isNumeric (value) {
-            return numericRegExp.test(String(value))
-        }
-
-        function toArray (value) {
-            if (!value) {
-                return []
-            }
-            
-            if (Array.isArray(value)) {
-                return value
-            }
-            
-            if (value instanceof window.NodeList || value instanceof window.HTMLCollection) {
-                return Array.prototype.slice.call(value)
-            }
-            
-            return [ value ]
-        }
-
-        function sortTable (table, ordering) {
-            console.log("second table")
-            var thead = table.querySelector('thead')
-            var tbody = table.querySelector('tbody')
-            var rows = toArray(tbody.rows)
-            var headers = toArray(thead.rows[0].cells)
-
-            var current = toArray(thead.querySelectorAll('.sorting_desc, .sorting_asc'))
-            
-            current.filter(function (item) { return !!item }).forEach(function (item) {
-                item.classList.remove('sorting_desc')
-                item.classList.remove('sorting_asc')
-            })
-            
-            headers.filter(function (item) { return !!item }).forEach(function (header) {
-                header.classList.remove('sorting_desc')
-                header.classList.remove('sorting_asc')
-            })
-            
-            ordering.forEach(function (order) {
-                var index = order.idx
-                var direction = order.dir || 'asc'
-                
-                var header = headers[index]
-                header.classList.add('sorting_' + direction)
-            })
-            
-            rows.sort(function sorter (a, b) {
-                var i = 0
-                var order = ordering[i]
-                var length = ordering.length
-                var aText
-                var bText
-                var result = 0
-                var dir
-                
-                while (order && result === 0) {
-                    dir = order.dir === 'desc' ? -1 : 1
-
-
-                    if(!a.cells[order.idx]){
-                        break;
-                    }
-
-                    if(!b.cells[order.idx]){
-                        break;
-                    }
-
-                    // if(!('textContent' in a.cells[order.idx])){
-                    //     break;
-                    // }
-
-                    // if(!('textContent' in b.cells[order.idx])){
-                    //     break;
-                    // }
-                    
-                    aText = a.cells[order.idx].textContent
-                    bText = b.cells[order.idx].textContent
-
-                    if (isNumeric(aText) && isNumeric(bText)) {
-                        result = dir * (parseFloat(aText) - parseFloat(bText))
-                    } else {
-                        result = dir * aText.localeCompare(bText)
-                    }
-                    
-                    i += 1
-                    order = ordering[i]
-                }
-                
-                return result
-            }).forEach(function each (row) {
-                tbody.appendChild(row)
-            })
-        }
-
-        function find (array, predicate) {
-            return toArray(array).filter(predicate)[0]
-        }
-
-        function initSortTable(table) {
-            var thead = table.querySelector('thead')
-            var ordering = [{idx:2,dir:'asc'},{idx:1,dir:'asc'}]
-            
-            table.__ordering = ordering
-            
-            thead.addEventListener('click', function onClick (event) {
-                var src = event.target || event.srcElement
-                var tagName = src.tagName.toLowerCase()
-                
-                if (tagName !== 'th') {
-                    return
-                }
-                
-                if (!event.shiftKey) {
-                    table.__ordering = [
-                        {
-                        idx: src.cellIndex,
-                        dir: src.classList.contains('sorting_asc') ? 'desc' : 'asc'
-                        }
-                    ]
-                } else {
-                    var order = find(table.__ordering, function (item) {
-                        return item.idx === src.cellIndex
-                    })
-                
-                    if (order) {
-                        order.dir = order.dir === 'asc' ? 'desc' : 'asc'
-                    } else {
-                        table.__ordering.push({
-                            idx: src.cellIndex,
-                            dir: 'asc'
-                        })
-                    }
-                }
-                
-                sortTable(table, table.__ordering)
-            }, false)
-        }
     }
 );
+
+function sendMessage(){
+    var getImageInfoButton = document.getElementsByClassName('get-image-info-button')[0];
+
+    getImageInfoButton.addEventListener('click', function() {
+
+        getImageInfoButton.disabled = true;
+
+        var params = {
+            active: true,
+            currentWindow: true
+        }
+
+        var messageTo = {
+            subject: "DOMInfo",
+            from: "imgs"
+        }
+
+        chrome.tabs.query(params, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, messageTo);
+        });
+
+    }, false);
+}
 
 function buildTableFromData(tableArray){
     var buildTable = "<table><thead><tr class=\"image-table-header\">";
@@ -459,4 +173,144 @@ function buildTableFromData(tableArray){
     
     buildTable += "</tr><tbody></table>";
     return buildTable;
+}
+
+//Helper functions for sorting
+function isNumeric (value) {
+    return numericRegExp.test(String(value))
+}
+
+function toArray (value) {
+    if (!value) {
+        return []
+    }
+    
+    if (Array.isArray(value)) {
+        return value
+    }
+    
+    if (value instanceof window.NodeList || value instanceof window.HTMLCollection) {
+        return Array.prototype.slice.call(value)
+    }
+    
+    return [ value ]
+}
+
+function sortTable (table, ordering) {
+    console.log("first table")
+    var thead = table.querySelector('thead')
+    var tbody = table.querySelector('tbody')
+    var rows = toArray(tbody.rows)
+    var headers = toArray(thead.rows[0].cells)
+
+    var current = toArray(thead.querySelectorAll('.sorting_desc, .sorting_asc'))
+    
+    current.filter(function (item) { return !!item }).forEach(function (item) {
+        item.classList.remove('sorting_desc')
+        item.classList.remove('sorting_asc')
+    })
+    
+    headers.filter(function (item) { return !!item }).forEach(function (header) {
+        header.classList.remove('sorting_desc')
+        header.classList.remove('sorting_asc')
+    })
+    
+    ordering.forEach(function (order) {
+        var index = order.idx
+        var direction = order.dir || 'asc'
+        
+        var header = headers[index]
+        header.classList.add('sorting_' + direction)
+    })
+    
+    rows.sort(function sorter (a, b) {
+        var i = 0
+        var order = ordering[i]
+        var length = ordering.length
+        var aText
+        var bText
+        var result = 0
+        var dir
+        
+        while (order && result === 0) {
+            dir = order.dir === 'desc' ? -1 : 1
+
+
+            if(!a.cells[order.idx]){
+                break;
+            }
+
+            if(!b.cells[order.idx]){
+                break;
+            }
+
+            // if(!('textContent' in a.cells[order.idx])){
+            //     break;
+            // }
+
+            // if(!('textContent' in b.cells[order.idx])){
+            //     break;
+            // }
+            
+            aText = a.cells[order.idx].textContent
+            bText = b.cells[order.idx].textContent
+
+            if (isNumeric(aText) && isNumeric(bText)) {
+                result = dir * (parseFloat(aText) - parseFloat(bText))
+            } else {
+                result = dir * aText.localeCompare(bText)
+            }
+            
+            i += 1
+            order = ordering[i]
+        }
+        
+        return result
+    }).forEach(function each (row) {
+        tbody.appendChild(row)
+    })
+}
+
+function find (array, predicate) {
+    return toArray(array).filter(predicate)[0]
+}
+
+function initSortTable (table) {
+    var thead = table.querySelector('thead')
+    var ordering = [{idx:2,dir:'asc'},{idx:1,dir:'asc'}]
+    
+    table.__ordering = ordering
+    
+    thead.addEventListener('click', function onClick (event) {
+        var src = event.target || event.srcElement
+        var tagName = src.tagName.toLowerCase()
+        
+        if (tagName !== 'th') {
+        return
+        }
+        
+        if (!event.shiftKey) {
+        table.__ordering = [
+            {
+            idx: src.cellIndex,
+            dir: src.classList.contains('sorting_asc') ? 'desc' : 'asc'
+            }
+        ]
+        } else {
+        var order = find(table.__ordering, function (item) {
+            return item.idx === src.cellIndex
+        })
+        
+        if (order) {
+            order.dir = order.dir === 'asc' ? 'desc' : 'asc'
+        } else {
+            table.__ordering.push({
+            idx: src.cellIndex,
+            dir: 'asc'
+            })
+        }
+        }
+        
+        sortTable(table, table.__ordering)
+    }, false)
 }
